15f6a9126428f69dc1b100a16e2d6af1058504cb
Mathias Markl <mathias@mukprojects.at>
2017-07-10 23:48:33 +0200

    muk_web_preview_video

---
+++ muk_web_preview_video/static/src/js/preview_generator.js
@@ -0,0 +1,67 @@
+/**********************************************************************************
+* 
+*    Copyright (C) 2017 MuK IT GmbH
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU Affero General Public License as
+*    published by the Free Software Foundation, either version 3 of the
+*    License, or (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU Affero General Public License for more details.
+*
+*    You should have received a copy of the GNU Affero General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+**********************************************************************************/
+
+odoo.define('muk_preview_video.PreviewGenerator', function (require) {
+"use strict";
+
+var core = require('web.core');
+
+var PreviewHandler = require('muk_preview.PreviewHandler');
+var PreviewGenerator = require('muk_preview.PreviewGenerator');
+
+var QWeb = core.qweb;
+var _t = core._t;
+
+var VideoHandler = PreviewHandler.extend({
+	mimetypeMap: {
+		'.mp4': 'video/mp4',
+		'.webm': 'video/webm',
+		'.ogg': 'video/ogg',
+		'mp4': 'video/mp4',
+		'webm': 'video/webm',
+		'ogg': 'video/ogg',
+	},
+	checkExtension: function(extension) {
+		return ['.mp4', '.webm', '.ogg', 'mp4', 'webm', 'ogg'].includes(extension);
+    },
+    checkType: function(mimetype) {
+		return ['video/mp4', '	video/webm', 'video/ogg'].includes(mimetype);
+    },
+    createHtml: function(url, mimetype, extension, title) {
+    	var result = $.Deferred();
+    	if(!mimetype) {
+    		mimetype = this.mimetypeMap[extension];
+    	}
+		var $content = $(QWeb.render('VideoHTMLContent', {url: url, type: mimetype}));
+        result.resolve($content);
+		return $.when(result);
+    },
+});
+
+PreviewGenerator.include({
+	videoHandler: {
+		"VideoHandler": new VideoHandler(),
+	},
+	init: function(additional_handler) {
+		additional_handler = _.extend(this.videoHandler, additional_handler);
+		this._super(additional_handler);
+	},
+});
+
+});
