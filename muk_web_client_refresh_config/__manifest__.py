f6747a316fbc893362e047691ae481ceda5607e9
Mathias Markl <mathias.christian.markl@gmail.com>
2017-09-12 13:25:32 +0200

    Update __manifest__.py

--- muk_web_client_refresh_config/__manifest__.py
+++ muk_web_client_refresh_config/__manifest__.py
@@ -27,7 +27,7 @@
         view refresh automatically. 
     """,
     "version": "10.0.1.0.0",
-    "category": "Extra Tools",
+    "category": "Hidden",
     "license": "AGPL-3",
     "website": "http://www.mukit.at",
     "author": "MuK IT",
