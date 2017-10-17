92530460046e967b696055854bc5972a8e9beb48
Mathias Markl <mathias.christian.markl@gmail.com>
2017-10-17 18:44:05 +0200

    Update __manifest__.py

--- muk_web_view_hierarchy/__manifest__.py
+++ muk_web_view_hierarchy/__manifest__.py
@@ -26,7 +26,7 @@
 
     """,
     "version": "10.0.1.0.1",
-    "category": "Extra Tools",
+    "category": "Hidden",
     "license": "AGPL-3",
     "website": "http://www.mukit.at",
     "author": "MuK IT",
