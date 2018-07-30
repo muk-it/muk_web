78f5abf8b56f11eef55c6cb37220b3435bb7c354
MuK IT GmbH <mathias.markl@mukit.at>
2018-07-30 17:27:01 +0000

    publish muk_web_share - 11.0.2.0.1

--- muk_web_share/__manifest__.py
+++ muk_web_share/__manifest__.py
@@ -20,7 +20,7 @@
 {
     "name": "MuK Web Share",
     "summary": """Share Button""",
-    "version": "11.0.2.0.0",
+    "version": "11.0.2.0.1",
     "category": "Extra Tools",
     "license": "AGPL-3",
     "website": "http://www.mukit.at",
