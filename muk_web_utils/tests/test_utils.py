fcc9d044aa6b800d8deac7eae5ba7dee021e53b4
MuK IT GmbH <mathias.markl@mukit.at>
2018-07-30 17:35:25 +0000

    publish muk_web_utils - 11.0.2.0.17

--- muk_web_utils/tests/test_utils.py
+++ muk_web_utils/tests/test_utils.py
@@ -18,6 +18,7 @@
 ###################################################################################
 
 import logging
+import unittest
 
 from odoo.tests import common
 
@@ -34,12 +35,15 @@ class UtilsTestCase(common.HttpCase):
     def tearDown(self):
         super(UtilsTestCase, self).tearDown()
     
+    @unittest.skip("PhantomJS")
     def test_common(self):
         self.phantom_js('/web/tests?filter=utils%20>%20common', "", "", login='admin', timeout=360)
         
+    @unittest.skip("PhantomJS")
     def test_jquery(self):
         self.phantom_js('/web/tests?filter=utils%20>%20jquery', "", "", login='admin', timeout=360)
     
+    @unittest.skip("PhantomJS")
     def test_mimetype(self):
         self.phantom_js('/web/tests?filter=utils%20>%20mimetype', "", "", login='admin', timeout=360)
         
