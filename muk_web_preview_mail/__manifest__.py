2cff278a9bab4b0e2cbfc77c9587950ef6e13242
MuK IT GmbH <mathias.markl@mukit.at>
2018-10-12 15:49:14 +0000

    publish muk_web_preview_mail - 11.0

--- muk_web_preview_mail/__manifest__.py
+++ muk_web_preview_mail/__manifest__.py
@@ -20,7 +20,7 @@
 {
     "name": "MuK Preview Mail",
     "summary": """Mail Preview""",
-    "version": "11.0.2.0.2",
+    "version": "11.0.2.0.3",
     "category": "Extra Tools",
     "license": "AGPL-3",
     "website": "http://www.mukit.at",
