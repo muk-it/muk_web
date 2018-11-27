cba9221fac946758b3589ef98532141d51cbfcda
MuK IT GmbH <mathias.markl@mukit.at>
2018-11-27 20:58:28 +0000

    publish muk_web_utils - 12.0

--- muk_web_utils/__manifest__.py
+++ muk_web_utils/__manifest__.py
@@ -20,7 +20,7 @@
 {
     "name": "MuK Web Utils",
     "summary": """Utility Features""",
-    "version": "12.0.2.4.10",
+    "version": "12.0.2.4.11",
     "category": "Extra Tools",
     "license": "AGPL-3",
     "author": "MuK IT",
