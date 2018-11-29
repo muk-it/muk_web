152bcb0e7919707489f6079283641a52ed6913b6
MuK IT GmbH <mathias.markl@mukit.at>
2018-11-29 02:49:54 +0000

    publish muk_web_utils - 12.0

--- muk_web_utils/doc/changelog.rst
+++ muk_web_utils/doc/changelog.rst
@@ -1,3 +1,8 @@
+`2.5.0`
+-------
+
+- Added color field widget
+
 `2.4.0`
 -------
 
---
+++ muk_web_utils/static/src/js/fields/color.js
@@ -0,0 +1,58 @@
+/**********************************************************************************
+* 
+*    Copyright (C) 2017 MuK IT GmbH
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU Affero General Public License as
+*    published by the Free Software Foundation, either version 3 of the
+*    License, or (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU Affero General Public License for more details.
+*
+*    You should have received a copy of the GNU Affero General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+**********************************************************************************/
+
+odoo.define('muk_web_utils.ColorChar', function (require) {
+"use strict";
+
+var core = require('web.core');
+var fields = require('web.basic_fields');
+var registry = require('web.field_registry');
+var colorpicker = require('web.colorpicker');
+
+var _t = core._t;
+var QWeb = core.qweb;
+
+var FieldColor = fields.DebouncedField.extend({
+	events: _.extend({}, fields.FieldChar.prototype.events, {
+		"click .mk_field_color_button": "_onCustomColorButtonClick",
+    }),
+    template: "muk_web_utils.FieldColor",
+    supportedFieldTypes: ['char'],
+    _renderEdit: function () {
+        this.$('.mk_field_color_input').val(this._formatValue(this.value));
+    },
+    _renderReadonly: function () {
+        this.$el.text(this._formatValue(this.value));
+    },
+    _onCustomColorButtonClick: function () {
+        var ColorpickerDialog = new colorpicker(this, {
+        	dialogClass: 'mk_field_color_picker'
+        });
+        ColorpickerDialog.on('colorpicker:saved', this, function (event) {
+        	this._setValue(event.data.hex);
+        });
+        ColorpickerDialog.open();
+    },
+});
+
+registry.add('color', FieldColor);
+
+return FieldColor;
+
+});
