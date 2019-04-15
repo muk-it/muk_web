4e9070586fbbb8029de406e1ec95cdae80fa5b31
MuK IT GmbH <mathias.markl@mukit.at>
2019-04-15 13:39:33 +0000

    publish muk_web_theme - 12.0

--- muk_web_theme/__manifest__.py
+++ muk_web_theme/__manifest__.py
@@ -19,7 +19,7 @@
 {
     "name": "MuK Backend Theme", 
     "summary": "Odoo Community Backend Theme",
-    "version": "12.0.1.4.8",
+    "version": "12.0.1.4.9",
     "category": "Themes/Backend",
     "license": "AGPL-3",
     "author": "MuK IT",
