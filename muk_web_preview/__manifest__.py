864ea3c3f598627bdbef20b39c7988a015ebf5c5
MuK IT GmbH <mathias.markl@mukit.at>
2019-07-13 23:36:34 +0000

    publish muk_web_preview - 12.0

--- muk_web_preview/__manifest__.py
+++ muk_web_preview/__manifest__.py
@@ -23,7 +23,7 @@
 {
     "name": "MuK Preview", 
     "summary": """File Preview Dialog""",
-    "version": "12.0.3.0.0",
+    "version": "12.0.3.0.1",
     "category": "Extra Tools",
     "license": "LGPL-3",
     "website": "http://www.mukit.at",
