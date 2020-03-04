27ce3d81ee3969beb7e4bacadd1ec418b5e952a3
Mathias Markl <mathias.markl@mukit.at>
2020-03-04 19:28:16 +0100

    Update __manifest__.py

--- muk_web_preview_rst/__manifest__.py
+++ muk_web_preview_rst/__manifest__.py
@@ -22,7 +22,7 @@
     "summary": """reStructuredText Preview""",
     "version": "11.0.1.0.2",
     "category": "Extra Tools",
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "website": "http://www.mukit.at",
     "live_test_url": "https://demo.mukit.at/web/login",
     "author": "MuK IT",
