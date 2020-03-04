f6c8655e1f98da6e632bbe882da0ce8dc2c1b82e
Mathias Markl <mathias.markl@mukit.at>
2020-03-04 19:28:40 +0100

    Update __manifest__.py

--- muk_web_glyphicons/__manifest__.py
+++ muk_web_glyphicons/__manifest__.py
@@ -22,7 +22,7 @@
     'summary': """Bootstrap Glyphicons Support""",
     "version": "11.0.2.0.1",
     "category": "Extra Tools",
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "website": "http://www.mukit.at",
     "live_test_url": "https://demo.mukit.at/web/login",
     "author": "MuK IT",
