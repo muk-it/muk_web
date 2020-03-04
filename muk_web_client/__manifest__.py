e3147fa2a935cff2c5e36179f14605b95d76bf62
Mathias Markl <mathias.markl@mukit.at>
2020-03-04 19:28:54 +0100

    Update __manifest__.py

--- muk_web_client/__manifest__.py
+++ muk_web_client/__manifest__.py
@@ -22,7 +22,7 @@
     "summary": """Odoo Web Client Extension""",
     "version": "11.0.2.0.2",
     "category": "Extra Tools",
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "website": "http://www.mukit.at",
     "live_test_url": "https://demo.mukit.at/web/login",
     "author": "MuK IT",
