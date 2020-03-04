1ca87654fbad2ec099a8c838cad6ba24441fe9df
Mathias Markl <mathias.markl@mukit.at>
2020-03-04 19:28:49 +0100

    Update __manifest__.py

--- muk_web_client_refresh/__manifest__.py
+++ muk_web_client_refresh/__manifest__.py
@@ -22,7 +22,7 @@
     "summary": """Web Client Refresh""",
     "version": "11.0.2.1.4",
     "category": "Extra Tools",
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "website": "http://www.mukit.at",
     "live_test_url": "https://demo.mukit.at/web/login",
     "author": "MuK IT",
