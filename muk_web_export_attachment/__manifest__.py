9b64e46bfa468c093acac9bae1300d4bd03ff6d4
Mathias Markl <mathias.markl@mukit.at>
2020-03-04 19:28:45 +0100

    Update __manifest__.py

--- muk_web_export_attachment/__manifest__.py
+++ muk_web_export_attachment/__manifest__.py
@@ -22,7 +22,7 @@
     "summary": """Export Odoo Attachments""",
     "version": "11.0.1.1.2",
     "category": "Extra Tools",
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "website": "http://www.mukit.at",
     "live_test_url": "https://demo.mukit.at/web/login",
     "author": "MuK IT",
