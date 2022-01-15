04ade6eaff0f6f45474434e0735fed6e42123cd7
Mathias Markl <mathias.markl@mukit.at>
2022-01-15 16:02:50 +0100

    publish muk_web_theme 15.0.1.0.1

--- muk_web_theme/__manifest__.py
+++ muk_web_theme/__manifest__.py
@@ -23,7 +23,7 @@
 {
     'name': 'MuK Backend Theme', 
     'summary': 'Odoo Community Backend Theme',
-    'version': '15.0.1.0.0', 
+    'version': '15.0.1.0.1', 
     'category': 'Themes/Backend', 
     'license': 'LGPL-3', 
     'author': 'MuK IT',
