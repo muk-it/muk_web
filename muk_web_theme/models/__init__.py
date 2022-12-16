97bd5c834bd4772ce23ce857107f3226fbcb3aae
Mathias Markl <mathias.markl@mukit.at>
2022-12-16 01:53:55 +0100

    publish muk_web_theme 16.0.1.0.4

--- muk_web_theme/models/__init__.py
+++ muk_web_theme/models/__init__.py
@@ -44,4 +44,6 @@ from . import res_company
 from . import res_config_settings
 from . import res_users
 from . import web_editor_assets
+from . import ir_attachment
+from . import ir_asset
 from . import ir_http
